9ecb95bebd97fcb205c823eb2de91bd7f1f0bf75
nol <smaccary@student.42.fr>
2022-06-02 01:22:04 +0200

    docs(SFINAE_examples): Added SFINAE examples and ressources

--- examples/SFINAE_dispatcher.cpp
+++ examples/SFINAE_dispatcher.cpp
@@ -3,28 +3,51 @@
 
 // Clean alternative to enable_if
 
-void to_be_resolved(ft::true_type) {
+
+/* FIRST METHOD (best) */
+// Avoids requirements issues
+
+void _to_be_resolved_dispatcher(ft::true_type) {
 	std::cout << "true\n";
 }
 
-void to_be_resolved(ft::false_type) {
+void _to_be_resolved_dispatcher(ft::false_type) {
 	std::cout << "false\n";
 }
-
 
 // We need to know our variable's type to use ft::is_integral on it.
 // Easier way to do this is to create a template dispactcher function this way:
 template<typename T>
-void to_be_resolved_dispatcher(const T & __x) {
+void to_be_resolved(const T & __x) {
 	(void) __x;
-	to_be_resolved(ft::is_integral<T>());
+	_to_be_resolved_dispatcher(ft::is_integral<T>());
+}
+
+/* SECOND METHOD (wrong)*/
+
+// This one will cause compile issue if T doesnt meet the requirements of one of the branches
+// (e.g.: iterators)
+template <typename T>
+void dynamic_dispatcher(const T &) {
+	if (ft::is_integral<T>()) { // Value replaced at compile time
+		std::cout << "true\n";
+	}
+	else
+		std::cout << "false\n";
 }
 
 int main(void)
 {
+	/* FIRST METHOD */
 	// Resolves to true
-	to_be_resolved_dispatcher(50);
+	to_be_resolved(50);
 
 	// Resolves to false
-	to_be_resolved_dispatcher(1.0);
+	to_be_resolved(1.0);
+
+	/* SECOND METHOD */
+
+	dynamic_dispatcher(50);
+
+	dynamic_dispatcher(1.0);
 }
